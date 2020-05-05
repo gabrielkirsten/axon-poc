e6fd0b6a054c3e563ee9abc104f13900a351d430
Gabriel Kirsten <gabriel.kirsten@hotmail.com>
2020-05-05 16:36:07 -0300

    first commit

---
+++ src/main/kotlin/gabrielkirsten/axonpoc/AxonPocApplication.kt
@@ -0,0 +1,11 @@
+package gabrielkirsten.axonpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AxonPocApplication
+
+fun main(args: Array<String>) {
+	runApplication<AxonPocApplication>(*args)
+}
---
+++ src/test/kotlin/gabrielkirsten/axonpoc/AxonPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package gabrielkirsten.axonpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AxonPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
